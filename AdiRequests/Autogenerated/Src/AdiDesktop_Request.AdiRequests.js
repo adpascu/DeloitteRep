71b441ee71317996d6093a8694d6bf8f89c0c864
adpascu <adrian.c.pascu@github.com>
2023-08-03 12:30:34 +0300

    test 3aug

---
+++ AdiRequests/Autogenerated/Src/AdiDesktop_Request.AdiRequests.js
@@ -0,0 +1,207 @@
+﻿define("AdiDesktop_Request", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Label_5u2gd0p",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 6,
+						"rowSpan": 1
+					},
+					"type": "crt.Label",
+					"caption": "#MacrosTemplateString(#ResourceString(Label_5u2gd0p_caption)#)#",
+					"labelType": "large-4",
+					"labelThickness": "bold",
+					"labelEllipsis": false,
+					"labelColor": "#FFFFFF",
+					"labelBackgroundColor": "transparent",
+					"labelTextAlign": "start",
+					"visible": true
+				},
+				"parentName": "FixedGridSlot_qwe4asds",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Button_gmjk7l2",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.Button",
+					"caption": "#ResourceString(Button_gmjk7l2_caption)#",
+					"color": "primary",
+					"disabled": false,
+					"size": "large",
+					"iconPosition": "only-text",
+					"visible": true,
+					"clicked": {
+						"request": "crt.CreateRecordRequest",
+						"params": {
+							"entityName": "AdiRequests"
+						}
+					},
+					"clickMode": "default"
+				},
+				"parentName": "FixedGridSlot_qwe4asds",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "ChartWidget_lk0me7h",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 6,
+						"rowSpan": 8
+					},
+					"type": "crt.ChartWidget",
+					"config": {
+						"title": "#ResourceString(ChartWidget_lk0me7h_title)#",
+						"color": "green",
+						"theme": "full-fill",
+						"scales": {
+							"stacked": false,
+							"xAxis": {
+								"name": "",
+								"formatting": {
+									"type": "string",
+									"maxLinesCount": 2,
+									"maxLineLength": 10
+								}
+							},
+							"yAxis": {
+								"name": "",
+								"formatting": {
+									"type": "number",
+									"thousandAbbreviation": {
+										"enabled": true
+									}
+								}
+							}
+						},
+						"series": [
+							{
+								"type": "doughnut",
+								"label": "#ResourceString(ChartWidget_lk0me7h_series_0)#",
+								"legend": {
+									"enabled": true
+								},
+								"data": {
+									"providing": {
+										"attribute": "ChartWidget_lk0me7h_fa9fdc04560f581d763ba8624a22c869",
+										"schemaName": "AdiExpenses",
+										"filters": {
+											"filter": {
+												"items": {
+													"6829625a-0c4e-48be-b7ed-9779d22a0eca": {
+														"filterType": 1,
+														"comparisonType": 3,
+														"isEnabled": true,
+														"trimDateTimeParameterToDate": false,
+														"leftExpression": {
+															"expressionType": 0,
+															"columnPath": "AdiRequest.AdiCode"
+														},
+														"isAggregative": false,
+														"dataValueType": 0,
+														"referenceSchemaName": "Contact",
+														"rightExpression": {
+															"expressionType": 1,
+															"functionType": 1,
+															"macrosType": 2
+														}
+													},
+													"columnIsNotNullFilter": {
+														"comparisonType": 2,
+														"filterType": 2,
+														"isEnabled": true,
+														"isNull": false,
+														"trimDateTimeParameterToDate": false,
+														"leftExpression": {
+															"expressionType": 0,
+															"columnPath": "AdiRequest.AdiSubcategory"
+														}
+													}
+												},
+												"logicalOperation": 0,
+												"isEnabled": true,
+												"filterType": 6,
+												"rootSchemaName": "AdiExpenses"
+											},
+											"filterAttributes": []
+										},
+										"aggregation": {
+											"column": {
+												"orderDirection": 0,
+												"orderPosition": -1,
+												"isVisible": true,
+												"expression": {
+													"expressionType": 1,
+													"functionArgument": {
+														"expressionType": 0,
+														"columnPath": "AdiAmount"
+													},
+													"functionType": 2,
+													"aggregationType": 2,
+													"aggregationEvalType": 0
+												}
+											}
+										},
+										"dependencies": [],
+										"rowCount": 50,
+										"grouping": {
+											"type": "by-value",
+											"column": {
+												"orderDirection": 0,
+												"orderPosition": -1,
+												"isVisible": true,
+												"expression": {
+													"expressionType": 0,
+													"columnPath": "AdiRequest.AdiSubcategory"
+												}
+											}
+										}
+									},
+									"formatting": {
+										"type": "number",
+										"decimalSeparator": ".",
+										"decimalPrecision": 2,
+										"thousandSeparator": ","
+									}
+								},
+								"dataLabel": {
+									"display": null
+								}
+							}
+						],
+						"seriesOrder": {
+							"type": "by-grouping-value",
+							"direction": 1
+						}
+					},
+					"sectionBindingColumnRecordId": "$Id"
+				},
+				"parentName": "FixedGridSlot_qwe4asds",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
---
+++ AdiRequests/Autogenerated/Src/AdiPage_k8rwtig.AdiRequests.js
@@ -0,0 +1,203 @@
+﻿define("AdiPage_k8rwtig", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "MainHeader",
+				"values": {
+					"alignItems": "flex-end"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "ActionButtonsContainer",
+				"values": {
+					"visible": true,
+					"color": "transparent",
+					"borderRadius": "none",
+					"padding": {
+						"top": "none",
+						"right": "none",
+						"bottom": "none",
+						"left": "none"
+					},
+					"justifyContent": "end",
+					"alignItems": "flex-end",
+					"gap": "small",
+					"wrap": "wrap"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "CloseButton",
+				"values": {
+					"caption": "#ResourceString(CloseButton_caption)#"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Checkbox_jz1wusg",
+				"values": {
+					"type": "crt.Checkbox",
+					"label": "$Resources.Strings.BooleanAttribute_dhclxwd_hgppfr0",
+					"labelPosition": "right",
+					"control": "$BooleanAttribute_dhclxwd_hgppfr0",
+					"visible": true,
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": true
+				},
+				"parentName": "ActionButtonsContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_bjhamx2",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_kao3yo2",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_kao3yo2",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_m2v7ifa",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_m2v7ifa_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_bjhamx2",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NumberInput_2tqnxh3",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.NumberAttribute_2v8jnpj",
+					"labelPosition": "above",
+					"control": "$NumberAttribute_2v8jnpj"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_8yjfe88",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_1dalbdg",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_1dalbdg",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "Input_rfis84r",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.StringAttribute_vzrndjg",
+					"labelPosition": "above",
+					"control": "$StringAttribute_vzrndjg"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 3
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"LookupAttribute_kao3yo2": {
+					"modelConfig": {
+						"path": "AdiExpensesDS.AdiRequest"
+					}
+				},
+				"NumberAttribute_2v8jnpj": {
+					"modelConfig": {
+						"path": "AdiExpensesDS.AdiAmount"
+					}
+				},
+				"StringAttribute_vzrndjg": {
+					"modelConfig": {
+						"path": "AdiExpensesDS.AdiComment"
+					}
+				},
+				"LookupAttribute_1dalbdg": {
+					"modelConfig": {
+						"path": "AdiExpensesDS.AdiType"
+					}
+				},
+				"BooleanAttribute_dhclxwd_hgppfr0": {
+					"modelConfig": {
+						"path": "AdiExpensesDS.AdiImported"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"AdiExpensesDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "page",
+					"config": {
+						"entitySchemaName": "AdiExpenses"
+					}
+				}
+			},
+			"primaryDataSourceName": "AdiExpensesDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
